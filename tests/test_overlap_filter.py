bef8372982cb3e6d61e8098baec2c7464b901b1e
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2021-04-16 15:17:12 +0100

    Refactored overlap filter and created test

---
+++ tests/test_overlap_filter.py
@@ -0,0 +1,37 @@
+import unittest
+import sys
+from rdkit import Chem
+from rdkit.Chem import rdmolfiles
+import numpy as np
+
+# import scripts
+sys.path.insert(1, '/home/sabsr3/xchem/fragment_network_merges/scripts')
+from overlap_filter import OverlapFilter
+
+# some test cases
+suppl = Chem.SDMolSupplier('tests/test_embedded_mols.sdf')
+mols = [x for x in suppl]
+passing_mol = mols[0]
+failing_mol = mols[1]
+proteinA = rdmolfiles.MolFromPDBFile('tests/Mpro-x0107_0A_apo-desolv.pdb')
+proteinB = rdmolfiles.MolFromPDBFile('tests/Mpro-x0678_0A_apo-desolv.pdb')
+
+passing_case = OverlapFilter(passing_mol, proteinA, proteinB)
+failing_case = OverlapFilter(failing_mol, proteinA, proteinB)
+
+class TestOverlapFilter(unittest.TestCase):
+
+    def test_calc_distances(self):
+        self.assertEqual((0.934984520123839, 0.9363730036063884), passing_case.calc_distances())
+    
+    def test_geometric_mean(self):
+        mean = np.sqrt(0.934984520123839 * 0.9363730036063884)
+        dists = passing_case.calc_distances()
+        self.assertEqual(mean, passing_case.geometric_mean(dists[0], dists[1]))
+    
+    def test_filter(self):
+        self.assertEqual(passing_mol, passing_case.filter())
+        self.assertIsNone(failing_case.filter())
+
+if __name__ == '__main__':
+    unittest.main()
