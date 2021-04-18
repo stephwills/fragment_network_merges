b21d6b910b6406b9bb1dee95af3ae544339ff620
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2021-04-18 21:07:05 +0100

    Commenting code

--- tests/test_embedding_filter.py
+++ tests/test_embedding_filter.py
@@ -1,10 +1,7 @@
+"""Tests the embedding filter script"""
 import unittest
-import sys
 from rdkit import Chem
-
-# import scripts
-sys.path.insert(1, '/home/sabsr3/xchem/fragment_network_merges/scripts')
-from embedding_filter import EmbeddingFilter
+from scripts.embedding_filter import EmbeddingFilter
 
 # some test cases
 passing_smiles = 'NC(=O)CN1CCC2(C1)CC1(C2)OCCO1'
@@ -15,13 +12,16 @@ fragmentB = Chem.MolFromMolFile('tests/Mpro-x0678_0A.mol')
 
 # finish writing unit tests
 class TestEmbeddingFilter(unittest.TestCase):
+    """Tests the EmbeddingFilter class"""
 
     def test_get_mcs(self):
+        """Checks that the MCS is correctly identified"""
         passing_case = EmbeddingFilter(passing_smiles, fragmentA, fragmentB, synthon)
         mcs = passing_case.get_mcs(Chem.MolFromSmiles(passing_smiles), fragmentA)
         self.assertEqual(Chem.MolToSmarts(mcs), '[#6](-[#6])-[#7]-[#6]:,-[#6](:,-[#6]:,-[#6])-[#6]')
 
     def test_filter(self):
+        """Checks that the filter correctly identifies molecules that can be embedded"""
         passing_case = EmbeddingFilter(passing_smiles, fragmentA, fragmentB, synthon).filter()
         failing_case = EmbeddingFilter(failing_smiles, fragmentA, fragmentB, synthon).filter()
         self.assertEqual(passing_case, 'pass')
