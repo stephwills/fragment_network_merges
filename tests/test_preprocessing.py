f21c38a1b4cad5b920b76d672e95a236447dfbd0
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2021-04-22 10:46:30 +0100

    Changed how script preprocesses fragments

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -9,7 +9,7 @@ class TestPreprocessing(unittest.TestCase):
     def test_get_smiles(self):
         """Tests smiles are retrieved correctly"""
         test_smi = get_smiles('Mpro', 'x0107', '0A')
-        smi = 'CC(=O)NC=1C=NC=CC1C'
+        smi = 'CC(=O)Nc1cnccc1C'
         self.assertEqual(test_smi, smi)
     
     def test_get_mol(self):
@@ -23,7 +23,7 @@ class TestPreprocessing(unittest.TestCase):
         """Tests the distance between fragments calculated OK"""
         test_fA = get_mol('Mpro', 'x0107', '0A')
         test_fB = get_mol('Mpro', 'x0434', '0A')
-        dist = 3.9218478311448566
+        dist = 0.39528976713292285
         self.assertEqual(dist, get_distance_between_fragments(test_fA, test_fB))
 
 if __name__ == '__main__':
