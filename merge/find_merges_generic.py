b4d4842193dae3ff03774e96a0a7009a34361767
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2022-02-15 12:01:25 +0000

    Modify merge code to improve querying, prevent redundant queries, better logging

--- merge/find_merges_generic.py
+++ merge/find_merges_generic.py
@@ -6,6 +6,8 @@ Uses code from https://github.com/tdudgeon/fragment-network-merges.
 import os
 import json
 import itertools
+import time
+import shutil
 
 from abc import ABC, abstractmethod
 from typing import List, Tuple
@@ -13,7 +15,7 @@ from rdkit import Chem
 from rdkit.Chem import rdShapeHelpers, Mol
 
 from merge.config_merge import config_merge
-from merge.preprocessing import get_mol
+from merge.preprocessing import get_mol, get_pair_dict, get_smiles, load_json
 from filter.embedding_filter import add_coordinates, remove_xe
 
 
@@ -198,7 +200,8 @@ class MergerFinder_generic(ABC):
 
         return filtered_synthons
 
-    def get_expansions(self, fragments:tuple, names:tuple, target:str, output_dir:str, synthons: list = None) -> dict:
+    def get_expansions(self, fragments:tuple, names:tuple, target:str, output_dir:str, synthons: list = None,
+                       fragalysis_dir = config_merge.FRAGALYSIS_DATA_DIR) -> dict:
         """
         Function executes the whole process, generating synthons for fragment B and using them to
         generate expansions of fragment A. Returns a dictionary containing all the synthons as keys,
@@ -222,7 +225,7 @@ class MergerFinder_generic(ABC):
         # get fragments A and B from the tuple
         fragmentA, fragmentB = fragments[0], fragments[1]
         nameA, nameB = names[0], names[1]
-        molA, molB = get_mol(target, nameA), get_mol(target, nameB)
+        molA, molB = get_mol(target, nameA, True), get_mol(target, nameB, True)
         print(f'Expanding fragment A: {nameA} with synthons of fragment B: {nameB}')
 
         # check if file already exists containing merges
@@ -278,6 +281,174 @@ class MergerFinder_generic(ABC):
                 json.dump(all_expansions, f)
 
         return all_expansions
+
+### NEW CODE TO PREVENT MAKING REDUNDANT QUERIES ###
+    def get_unique_synthons(self, nameA: str, nameBs: list, target:str) -> Tuple[list, dict]:
+        """
+        For a given fragment A, get the synthons for each fragment B and thus the unique synthons across all pairs
+        (several synthons will be the same for different fragment Bs, so we don't want to re-run these queries).
+        Returns a list of the unique synthons, and the synthon dict.
+
+        :param nameA: fragment A name, e.g. x0034_0B
+        :type nameA: str
+        :param nameBs: list of fragment B names, e.g. ['x0034_0B', 'x0176_0B']
+        :type nameBs: list
+        :param target: name of target, e.g. 'nsp13'
+        :type target: str
+
+        :return: list of all the unique synthons, synthon dictionary with fragB as key and synthons as vals
+        :rtype: tuple
+        """
+        all_synthons = set()  # store all unique synthons
+        synthon_dict = {}  # store the synthons associated with each fragment B
+        total_synthons = 0  # record total synthons
+        molA = get_mol(target, nameA, True)  # get mol for fragment A
+
+        for nameB in nameBs:
+            print(f'Generating synthons: fragment A: {nameA}; fragment B: {nameB}')
+
+            # get fragment B smiles and mol
+            fragmentB = get_smiles(target, nameB)
+            molB = get_mol(target, nameB, True)
+
+            # generate the synthons from fragment B
+            synthons = self.get_synthons(fragmentB)
+
+            # filter synthons
+            synthons = self.carbons_check(synthons)  # filter by number of carbons
+            synthons = self.substructure_check(synthons, molA, molB)  # filter by whether in fragment A
+            print(f'{len(synthons)} synthons remaining after filtering')
+            total_synthons += len(synthons)
+            # record info
+            synthon_dict[nameB] = synthons
+            for s in synthons:
+                all_synthons.add(s)
+
+        print(f"{len(all_synthons)} unique synthons for expansion of {nameA} (out of {total_synthons} total)")
+
+        return list(all_synthons), synthon_dict
+
+    def get_all_expansions(self, nameA, all_synthons: list, target: str, output_dir=None,
+                           working_dir=config_merge.WORKING_DIR) -> dict:
+        """
+        For a given fragment A and all the synthons generated from the fragment B pairs, function queries the network
+        to identify all expansions for each synthon. Returns a dictionary with the synthon as the key and list of
+        expansions as the value.
+
+        :param nameA: fragment A name, e.g. x0034_0B
+        :type nameA: str
+        :param all_synthons: list of unique synthons for expansion
+        :type all_synthons: list
+        :param target: name of target, e.g. 'nsp13'
+        :type target: str
+        :param output_dir: output directory
+        :type output_dir: str
+        :param working_dir: working directory
+        :type working_dir: str
+
+        :return: dictionary with synthon as key and expansions as values
+        :rtype: dict
+        """
+        # check if there is an intermediate file
+        fname = os.path.join(working_dir, f"{nameA}_expansions.json")
+        if os.path.exists(fname):
+            print(f'Expansions have already been generated for {nameA}')
+            all_expansions = load_json(fname)
+        else:
+            all_expansions = {}
+
+        # check if there is an intermediate file for recording timings
+        timings_fname = os.path.join(working_dir, f"{nameA}_timings.json")
+        if os.path.exists(timings_fname):
+            timings = load_json(timings_fname)
+        else:
+            timings = {}
+
+        # run database query and expansion
+        fragA = get_smiles(target, nameA)
+        print(f"Generating expansions for fragment {nameA}")
+        print(fragA, all_synthons)
+        # run queries
+        with self.getSearchSession() as session:
+            number = 0  # keep track of synthon number
+            total_expansions = 0
+            expanded_synthons = 0
+            for synthon in all_synthons:  # expand fragment A using each synthon
+                print(f'Running synthon {number}/{len(all_synthons)}')
+                start = time.time()
+                # skip synthon if results already generated
+                if synthon in all_expansions:
+                    print(f'Results already generated for synthon {synthon}')
+                    expansions = all_expansions[synthon]
+                else:
+                    expansions = session.find_expansions(fragA, synthon)
+                    all_expansions[synthon] = list(expansions)  # store in dictionary with the synthon as key
+                print(f'Found {len(expansions)} expansions')
+                number += 1
+                total_expansions += len(expansions)
+                if expansions:  # record if the synthon led to expansions
+                    expanded_synthons += 1
+                end = time.time()
+                if synthon not in timings:
+                    synthon_time = round(end-start, 2)
+                    timings[synthon] = synthon_time
+
+                # write to file after each query
+                with open(fname, 'w') as f:
+                    json.dump(all_expansions, f)
+
+                # write timings to file after each query
+                with open(timings_fname, 'w') as f:
+                    json.dump(timings, f)
+        print(f'{total_expansions} expansions from {expanded_synthons} out of {len(all_synthons)} synthons')
+        # record total time taken
+        total_time = sum(timings.values())
+        timings['total_time'] = total_time
+        with open(timings_fname, 'w') as f:
+            json.dump(timings, f)
+        if output_dir:
+            shutil.move(timings_fname, os.path.join(output_dir, f"{nameA}_timings.json"))
+
+        return all_expansions
+
+    def expand_fragmentA(self, name_pairs, target,  output_dir=None, working_dir=config_merge.WORKING_DIR):
+        """
+        Function is given the whole list of pairs of enumerated fragments and generates all merges.
+        Creates files in the output directory containing the expansions for each pair.
+
+        :param name_pairs: list of enumerated fragment pairs, e.g. [['x0034_0B', 'x0176_0B'], ['x0176_0B', 'x0034_0B']]
+        :type name_pairs: list
+        :param target: name of target, e.g. 'nsp13'
+        :type target: str
+        :param output_dir: output directory
+        :type output_dir: str
+        :param working_dir: working directory
+        :type working_dir: str
+        """
+        # check for working dir
+        if not os.path.exists(working_dir):
+            os.mkdir(working_dir)
+
+        # get fragBs to be used for expanding each fragA
+        pair_dict = get_pair_dict(name_pairs)
+
+        for nameA in pair_dict:
+            nameBs = pair_dict[nameA]
+            unique_synthons, synthon_dict = self.get_unique_synthons(nameA, nameBs, target)
+            all_expansions = self.get_all_expansions(nameA, unique_synthons, target, output_dir, working_dir)
+
+            for nameB in nameBs:
+                # save a file containing expansions for each fragment pair
+                dict_to_save = {}
+                synthons = synthon_dict[nameB]
+                for synthon in synthons:
+                    dict_to_save[synthon] = all_expansions[synthon]
+
+                if output_dir:
+                    fname = nameA + '_' + nameB + '.json'
+                    fname = os.path.join(output_dir, fname)
+                    with open(fname, 'w') as f:
+                        json.dump(dict_to_save, f)
 
 
 def add_required_synthons(labels:set, synthon:str):
