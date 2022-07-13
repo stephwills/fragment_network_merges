a2324f87a6d6d79db9be750feebe3085e229e19f
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2022-07-13 15:04:05 +0100

    better config

--- utils/parallelUtils.py
+++ utils/parallelUtils.py
@@ -1,17 +1,10 @@
 import os
 import re
-
 import dask
-
-from similaritySearch.similaritySearchConfig import N_CPUS, DASK_WORKER_MEMORY, TMP_DIR, DISABLE_DASHBOARD
-
-import os
+import similaritySearch.similaritySearchConfig as config
 import logging
 
 from dask.distributed import Client, LocalCluster
-
-
-# DASK_DISTRIBUTED__COMM__TIMEOUTS__CONNECT="20" #Default is 10
 
 journal = logging.getLogger('Dask_Parallel')
 journal.setLevel(logging.DEBUG)
@@ -22,19 +15,19 @@ def get_parallel_client(threads_per_worker=None, n_workers=None, memory_limit=No
     global DASK_CLIENT
     if DASK_CLIENT is None:
         if n_workers is None:
-            n_workers = N_CPUS
+            n_workers = config.N_CPUS
             threads_per_worker = 1
         if memory_limit is None:
-            memory_limit= DASK_WORKER_MEMORY
+            memory_limit= config.DASK_WORKER_MEMORY
             if memory_limit == "-1":
                 from psutil import virtual_memory
                 mem = virtual_memory()
                 if mem.total is None:
                     raise ValueError("Error, memory was not determined")
                 memory_limit="%dGB"%( (0.9 *mem.total/n_workers) // 2 ** 30)
-        dask.config.set({'temporary_directory': os.path.join(TMP_DIR, "dask")})
+        dask.config.set({'temporary_directory': os.path.join(config.TMP_DIR, "dask")})
         if n_workers>1 or threads_per_worker>1:
-            if DISABLE_DASHBOARD:
+            if config.DISABLE_DASHBOARD:
                 kwargs = {"dashboard_address":None}
             else:
                 kwargs = {}
@@ -42,7 +35,7 @@ def get_parallel_client(threads_per_worker=None, n_workers=None, memory_limit=No
         else:
             DASK_CLIENT = Client( LocalCluster(threads_per_worker=1, n_workers=1) )
         print(DASK_CLIENT, flush=True)
-        if not DISABLE_DASHBOARD:
+        if not config.DISABLE_DASHBOARD:
             print(DASK_CLIENT.scheduler_info()['services'])
     return DASK_CLIENT
 
@@ -52,7 +45,7 @@ def apply_func_to_files(folder, file_pattern, function, use_parallel_dask=None, 
     results = []
 
     if use_parallel_dask is None:
-        use_parallel_dask = N_CPUS > 0
+        use_parallel_dask = config.N_CPUS > 0
 
     if use_parallel_dask:
         function = dask.delayed(function)
